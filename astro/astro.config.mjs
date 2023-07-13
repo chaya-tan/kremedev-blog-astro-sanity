f13a602a3ac68b4db18de6cb68a08c13050fb089
chaya-tan <chayatan.ang@gmail.com>
2023-07-13 21:32:05 +0700

    changed sanity id

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-import sanity from 'astro-sanity';
+import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-sanity-minimal-starter.netlify.app/',
+  site: "https://astro-sanity-minimal-starter.netlify.app/",
   // Your public domain, e.g.: https://my-site.dev/
   integrations: [
     sitemap(),
     sanity({
-      projectId: '8hj1t7km',
-      dataset: 'production',
-      apiVersion: 'v2021-03-25',
+      projectId: "8yiyn93s",
+      // projectId: "8hj1t7km", // example from template
+      dataset: "production",
+      apiVersion: "v2021-03-25",
       useCdn: true,
     }),
   ],
