caa48cf32ae88611d4fbae80ceb197bb72d63cc0
chaya-tan <chayatan.ang@gmail.com>
2023-09-17 02:14:32 +0700

    added main page design

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import sanity from "astro-sanity";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sanity-minimal-starter.netlify.app/",
   // Your public domain, e.g.: https://my-site.dev/
-  integrations: [
-    sitemap(),
-    sanity({
-      projectId: "8yiyn93s",
-      // projectId: "8hj1t7km", // example from template
-      dataset: "production",
-      apiVersion: "v2021-03-25",
-      useCdn: true,
-    }),
-  ],
+  integrations: [sitemap(), sanity({
+    projectId: "8yiyn93s",
+    // projectId: "8hj1t7km", // example from template
+    dataset: "production",
+    apiVersion: "v2021-03-25",
+    useCdn: true
+  }), tailwind()]
 });
